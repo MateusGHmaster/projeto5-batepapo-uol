bc013d9851bfd60c5562d8b915f699f8072d366c
Mateus <mateusoliveira469@gmail.com>
2022-02-09 11:20:44 -0400

    get-local-username

---
+++ script.js
@@ -0,0 +1,7 @@
+let userName = null;
+
+function insertUsername () {
+    userName = prompt("Por favor, insira seu nome de usuário, para que possamos logo ingressar você em uma conversa! ♥(ˆ⌣ˆԅ)");
+}
+
+insertUsername();
