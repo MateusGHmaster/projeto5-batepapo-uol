c9eb5d8b704e2bb038a697eb90d55c5a6a738316
Mateus <mateusoliveira469@gmail.com>
2022-02-09 15:42:51 -0400

    bugged-promise-request

--- script.js
+++ script.js
@@ -4,4 +4,21 @@ function insertUsername () {
     userName = prompt("Por favor, insira seu nome de usuário, para que possamos logo ingressar você em uma conversa! ♥(ˆ⌣ˆԅ)");
 }
 
+/* const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/participants");
+promise.then(processResponse); */
+
+/* function processResponse(response) {
+    console.log(response);
+} */
+
+
+
+
+
+
+
+
+
+
+
 insertUsername();
