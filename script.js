44097b992ec130c52a6a25cbabc0b92739e22398
Mateus <mateusoliveira469@gmail.com>
2022-02-10 13:36:06 -0400

    fixed-promise-request

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ function insertUsername () {
     userName = prompt("Por favor, insira seu nome de usuário, para que possamos logo ingressar você em uma conversa! ♥(ˆ⌣ˆԅ)");
 }
 
-/* const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/participants");
-promise.then(processResponse); */
+const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/participants");
+promise.then(processResponse);
 
-/* function processResponse(response) {
+function processResponse(response) {
     console.log(response);
-} */
+} 
 
 
 
